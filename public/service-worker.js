9be56fe8b3bb669a16c5ff70d6325404f8a9feee
Vanessa Maldonado <vbarajas4@gmail.com>
2021-07-26 18:57:26 -0700

    fixed a typo in styles file

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
     "/db.js",
     "/index.js",
-    "/style.css",
+    "/styles.css",
     "/manifest.webmanifest",
     "/index.html"
   ];
